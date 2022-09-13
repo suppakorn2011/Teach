c6ab3aedb2f2d4612a87af2f5ee9bb9d1d0eebc6
suppakorn2011 <113385951+suppakorn2011@users.noreply.github.com>
2022-09-13 19:18:07 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,78 @@
+let Set = 0
+input.onButtonPressed(Button.A, function () {
+    Set = randint(0, 34)
+    if (Set == 1) {
+        basic.showNumber(1)
+    } else if (Set == 2) {
+        basic.showNumber(2)
+    } else if (Set == 3) {
+        basic.showNumber(3)
+    } else if (Set == 4) {
+        basic.showNumber(4)
+    } else if (Set == 5) {
+        basic.showNumber(5)
+    } else if (Set == 6) {
+        basic.showNumber(6)
+    } else if (Set == 7) {
+        basic.showNumber(7)
+    } else if (Set == 8) {
+        basic.showNumber(8)
+    } else if (Set == 9) {
+        basic.showNumber(9)
+    } else if (Set == 10) {
+        basic.showNumber(10)
+    } else if (Set == 11) {
+        basic.showNumber(11)
+    } else if (Set == 12) {
+        basic.showNumber(12)
+    } else if (Set == 13) {
+        basic.showNumber(13)
+    } else if (Set == 14) {
+        basic.showNumber(14)
+    } else if (Set == 15) {
+        basic.showNumber(15)
+    } else if (Set == 16) {
+        basic.showNumber(16)
+    } else if (Set == 17) {
+        basic.showNumber(17)
+    } else if (Set == 18) {
+        basic.showNumber(18)
+    } else if (Set == 19) {
+        basic.showNumber(19)
+    } else if (Set == 20) {
+        basic.showNumber(20)
+    } else if (Set == 21) {
+        basic.showNumber(21)
+    } else if (Set == 22) {
+        basic.showNumber(22)
+    } else if (Set == 23) {
+        basic.showNumber(23)
+    } else if (Set == 24) {
+        basic.showNumber(24)
+    } else if (Set == 25) {
+        basic.showNumber(25)
+    } else if (Set == 26) {
+        basic.showNumber(26)
+    } else if (Set == 27) {
+        basic.showNumber(27)
+    } else if (Set == 28) {
+        basic.showNumber(28)
+    } else if (Set == 29) {
+        basic.showNumber(29)
+    } else if (Set == 30) {
+        basic.showNumber(30)
+    } else if (Set == 31) {
+        basic.showNumber(31)
+    } else if (Set == 32) {
+        basic.showNumber(32)
+    } else if (Set == 33) {
+        basic.showNumber(33)
+    } else if (Set == 34) {
+        basic.showNumber(34)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+})
